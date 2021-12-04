625046f0713c361cd85a07e76c1aad25796c4d8b
AshenRed <45851006+AshenRed@users.noreply.github.com>
2021-12-04 20:28:41 +0100

    added click command pipline

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import os
 import torch
+import click
 import torch.nn as nn
 from torch.nn.utils.rnn import pad_sequence
 import torch.utils.data as data
@@ -17,87 +19,56 @@ torch.manual_seed(33)
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-def my_collate(batch):
+############################################################################################################################################################################################################################################################
+# Train: 
+# python main.py -l en -t -e 10 -fi -fd Data\\en\\gold\\dev.txt -f Data\\en\\gold\\test.txt -ft Data\\en\\gold\\train.txt
+# python main.py -l en -b 12 -t -e 10 -w 0 -lr 0.00015 -fi -s -fm Data\\en\\model_paremeters.pth -fd Data\\en\\gold\\dev.txt -f Data\\en\\gold\\test.txt -ft Data\\en\\gold\\train.txt -f3 Data\\en\\silver\\train.txt -f4 Data\\en\\bronze\\train.txt
+# Test: 
+# python main.py -l en -fi -fm Data\\en\\model_paremeters.pth -fd Data\\en\\gold\\dev.txt -f Data\\en\\gold\\test.txt -ft Data\\en\\gold\\train.txt -f3 Data\\en\\silver\\train.txt -f4 Data\\en\\bronze\\train.txt
+############################################################################################################################################################################################################################################################
 
-    input_ids = [item[0] for item in batch]
-    token_type_ids = [item[1] for item in batch]
-    attention_mask = [item[2] for item in batch]
-    valid_indices = [item[3] for item in batch]
-                
-    input_ids = pad_sequence(input_ids, batch_first=True, padding_value=0.0)
-    token_type_ids = pad_sequence(token_type_ids, batch_first=True, padding_value=0.0)
-    attention_mask = pad_sequence(attention_mask, batch_first=True, padding_value=0.0)
+@click.group()
+def main():
+    pass
 
-    bert_input = {'input_ids':input_ids, 'token_type_ids':token_type_ids, 'attention_mask':attention_mask}
-
-    target_s = [torch.LongTensor(item[4]).to(device) for item in batch]
-    target_f = [torch.LongTensor(item[5]).to(device) for item in batch]
-    target_i = [torch.LongTensor(item[6]).to(device) for item in batch]
-    orgn_sent = [item[7] for item in batch]
-
-    return bert_input, valid_indices, target_s, target_f, target_i, orgn_sent
-
-# def my_collate(batch): #version that excludes unaligned sentences
-
-#     input_ids = []
-#     token_type_ids = []
-#     attention_mask = []
-#     valid_indices = []
-#     target_s = []
-#     target_f = []
-#     target_i = []
-#     sent = []
-
-#     for item in batch: 
-#         if len(item[3])==len(item[7])+1:
-#             input_ids.append(item[0])
-#             token_type_ids.append(item[1])
-#             attention_mask.append(item[2])
-#             valid_indices.append(item[3])
-#             target_s.append(torch.LongTensor(item[4]).to(device))
-#             target_f.append(torch.LongTensor(item[5]).to(device))
-#             target_i.append(torch.LongTensor(item[6]).to(device))
-#             sent.append(item[7])
-                
-#     input_ids = pad_sequence(input_ids, batch_first=True, padding_value=0.0)
-#     token_type_ids = pad_sequence(token_type_ids, batch_first=True, padding_value=0.0)
-#     attention_mask = pad_sequence(attention_mask, batch_first=True, padding_value=0.0)
-
-#     bert_input = {'input_ids':input_ids, 'token_type_ids':token_type_ids, 'attention_mask':attention_mask}
-
-#     return bert_input, valid_indices, target_s, target_f, target_i, sent
-
-
-def average_word_emb(emb, valid):
-    embs = []
-    for i in range(len(valid)-1):
-        embs.append(emb[torch.LongTensor([idx for idx in range(valid[i], valid[i+1])]).to(device)].mean(0).unsqueeze(0))
-    return torch.cat(embs, 0)
-
-if __name__ == '__main__':
-
-    #train
-    lang = "en"
-
-    train = True
-
-    save_checkpoint = True
-
-    hyper_batch_size = 16
-
-    num_warmup_steps = 0
-
-    learning_rate = 0.00015
-
-    epochs = 10
-    middle_epoch = 5
+@click.command("language")
+@click.option("-l", "--language",type=click.Choice(['en', 'de','it', 'nl'], case_sensitive=False), help="en: English, de: German, it: Italian, nl: Dutch")
+@click.option('-b','--batch', type=int, default=16, help="Batch size")
+@click.option('-t','--train', is_flag=True, help='Train: true | Test: false')
+@click.option('-e','--epoch', type=int, default=10, help="Training + Testing, or Test only")
+@click.option('-w','--num_warmup_steps', type=int, default=0, help="Number of warm_up steps in training for linear scheduler")
+@click.option('-lr','--learning_rate', type=float, default=0.00015, help="Learning rate of training")
+@click.option('-fi','--finetuning', is_flag=True, help="Train BERT parameters or not")
+@click.option('-s','--save_checkpoint', is_flag=True, help='True: save, False: no save')
+@click.option('-fm','--model_file', type=click.Path(exists=True), required=False, help="Provide model file path, and choose whether to save model. ")
+@click.option('-fd','--dev_file', type=click.Path(exists=True), help="Dev file path (optional)")
+@click.option('-f','--test_file', type=click.Path(exists=True), required=False, default=None, help="Test file path")
+@click.option('-ft','--train_file', type=click.Path(exists=True), help="Train file path")
+@click.option('-f3','--train_file_op1', type=click.Path(exists=True), required=False, default=None, help="Optional 2nd train set path for train & finetuning")
+@click.option('-f4','--train_file_op2', type=click.Path(exists=True), required=False, default= None, help="Optional 3rd train set path for train & finetuning")
+def config(language, batch, train, epoch, num_warmup_steps, learning_rate, finetuning, save_checkpoint, model_file, dev_file, test_file, train_file, train_file_op1, train_file_op2):
+    
+    middle_epoch = epoch/2
     old_epoch = 0
-    if epochs < middle_epoch:
-        middle_epoch = epochs 
 
     bert_embed_size = 768
 
-    fine_tune  = True
+    if train_file_op1==None and train_file_op2==None:
+        middle_epoch = epoch
+ 
+    f = open(dev_file)
+    root_dir=os.path.realpath(f.name)
+    f.close()
+    in_files = [dev_file]
+    out_files = [root_dir+'prediction_dev.txt']
+    out_files2 = [root_dir+'sen_prpty_dev.txt', ]
+    
+    if dev_file != None:
+        in_files.append(test_file)
+        out_files.append(root_dir+'prediction_test.txt')
+        out_files2.append(root_dir+'sen_prpty_test.txt')
+
+
 
     start = torch.cuda.Event(enable_timing=True)
     end = torch.cuda.Event(enable_timing=True)
@@ -105,15 +76,15 @@ if __name__ == '__main__':
     start.record()
     
     # for en, de:
-    words, senses, fragment, integration_labels, tr_sents, tr_targets, content_frg_idx, orgn_sents, sents2, targets2 = preprocess.encode2(primary_file ='Data\\'+lang+'\\gold\\train.txt', optional_file='Data\\'+lang+'\\silver\\train.txt', optional_file2='Data\\'+lang+'\\bronze\\train.txt', language=lang)
+    words, senses, fragment, integration_labels, tr_sents, tr_targets, content_frg_idx, orgn_sents, sents2, targets2 = preprocess.encode2(primary_file = train_file, optional_file = train_file_op1, optional_file2 = train_file_op2, language=language)
     # for it, nl:
     # words, senses, fragment, integration_labels, tr_sents, tr_targets, content_frg_idx, orgn_sents, sents2, targets2 = preprocess.encode2(primary_file ='Data\\'+lang+'\\silver\\train.txt', optional_file='Data\\'+lang+'\\bronze\\train.txt', optional_file2=None, language=lang)
     
     bert_models = {"en": "bert-base-cased","nl": "Geotrend/bert-base-nl-cased", "de": "dbmdz/bert-base-german-cased", "it": "dbmdz/bert-base-italian-cased"}
 
-    model_path = 'Data\\'+lang+'\\model_paremeters.pth' # path of checkpoint of finetuned model
+    #model_path = 'Data\\'+lang+'\\model_paremeters.pth' # path of checkpoint of finetuned model
 
-    model_name = bert_models[lang]
+    model_name = bert_models[language]
     
     tokenizer = AutoTokenizer.from_pretrained(model_name)
 
@@ -134,9 +105,9 @@ if __name__ == '__main__':
     bert_model.config.output_hidden_states=True
 
     
-    primary_loader = data.DataLoader(dataset=train_dataset, batch_size=hyper_batch_size, sampler=primary_sampler, shuffle=False, collate_fn=my_collate)
+    primary_loader = data.DataLoader(dataset=train_dataset, batch_size=batch, sampler=primary_sampler, shuffle=False, collate_fn=my_collate)
     if len(train_dataset)>train_dataset.primary_size:
-        optional_loader = data.DataLoader(dataset=train_dataset, batch_size=hyper_batch_size, sampler=optional_sampler, shuffle=False, collate_fn=my_collate)
+        optional_loader = data.DataLoader(dataset=train_dataset, batch_size=batch, sampler=optional_sampler, shuffle=False, collate_fn=my_collate)
 
     lossfunc = nn.CrossEntropyLoss()
 
@@ -167,8 +138,8 @@ if __name__ == '__main__':
     ################ Load Checkpoints ###############################################################
     if (train == True and save_checkpoint == True) or (train == False and save_checkpoint == False):
         try:
-            checkpoint = torch.load(model_path, map_location=device)
-            if fine_tune == True:
+            checkpoint = torch.load(model_file, map_location=device)
+            if finetuning == True:
                 bert_model.load_state_dict(checkpoint['bert_model_state_dict'])
                 bert_optimizer.load_state_dict(checkpoint['bert_optimizer_state_dict'])
             tagging_model.load_state_dict(checkpoint['tagging_model_state_dict'])       
@@ -182,24 +153,24 @@ if __name__ == '__main__':
 
     if train:
         scheduler1 = get_linear_schedule_with_warmup(optimizer, num_warmup_steps, len(train_dataset)*middle_epoch)
-        scheduler3 = get_linear_schedule_with_warmup(optimizer, num_warmup_steps, train_dataset.primary_size*(epochs - middle_epoch))
-        if fine_tune == True:
+        scheduler3 = get_linear_schedule_with_warmup(optimizer, num_warmup_steps, train_dataset.primary_size*(epoch - middle_epoch))
+        if finetuning == True:
             scheduler2 = get_linear_schedule_with_warmup(bert_optimizer, num_warmup_steps, len(train_dataset)*middle_epoch)
-            scheduler4 = get_linear_schedule_with_warmup(bert_optimizer, num_warmup_steps, train_dataset.primary_size*(epochs - middle_epoch))
+            scheduler4 = get_linear_schedule_with_warmup(bert_optimizer, num_warmup_steps, train_dataset.primary_size*(epoch - middle_epoch))
 
         with torch.no_grad():
             content_set = torch.LongTensor(list(train_dataset.content_frg_idx)).to(device)
 
-        for e in range(epochs):
+        for e in range(epoch):
             total_loss = 0.0
-            if e >= middle_epoch or len(train_dataset)==train_dataset.primary_size or epochs==middle_epoch:
+            if e >= middle_epoch or len(train_dataset)==train_dataset.primary_size or epoch==middle_epoch:
                 train_loader = primary_loader
             else:
                 train_loader = optional_loader
 
             for idx, (bert_input, valid_indices, target_s, target_f, target_i, og_sents) in enumerate(train_loader):
 
-                if fine_tune == False:
+                if finetuning == False:
                     with torch.no_grad():
                         bert_outputs = bert_model(**bert_input)
 
@@ -248,7 +219,7 @@ if __name__ == '__main__':
                     batch_loss = batch_loss + sense_loss + frg_loss + inter_loss
 
                 optimizer.zero_grad()
-                if fine_tune == True:
+                if finetuning == True:
                     bert_optimizer.zero_grad()
                 batch_loss.backward()
                 optimizer.step()
@@ -256,7 +227,7 @@ if __name__ == '__main__':
                     scheduler1.step()
                 else:
                     scheduler3.step()
-                if fine_tune == True:
+                if finetuning == True:
                     bert_optimizer.step()
                     if e <middle_epoch:
                         scheduler2.step()
@@ -277,16 +248,13 @@ if __name__ == '__main__':
             'tagging_model_state_dict': tagging_model.state_dict(),
             'bert_optimizer_state_dict': bert_optimizer.state_dict(),
             'tagging_optimizer_state_dict': optimizer.state_dict(),
-            }, model_path)
+            }, model_file)
 
     end.record()
     torch.cuda.synchronize()
     print("Train time: ",start.elapsed_time(end))  
 
     #eval:
-    in_files = ['Data\\'+lang+'\\gold\\dev.txt', 'Data\\'+lang+'\\gold\\test.txt']
-    out_files = ['Data\\'+lang+'\\gold\\prediction_dev.txt', 'Data\\'+lang+'\\gold\\prediction_test.txt']
-    out_files2 = ['Data\\'+lang+'\\gold\\sen_prpty_dev.txt', 'Data\\'+lang+'\\gold\\sen_prpty_test.txt']
     for in_f, out_f, out_f2 in zip(in_files, out_files, out_files2):
         with torch.no_grad():
 
@@ -298,13 +266,13 @@ if __name__ == '__main__':
             correct_i = 0
             n_of_t = 0
             count = 1
-            _, _, _, _, te_sents, te_targets, _, orgn_sents, _, _ = preprocess.encode2(primary_file = in_f, language = lang)
+            _, _, _, _, te_sents, te_targets, _, orgn_sents, _, _ = preprocess.encode2(primary_file = in_f, language = language)
             pred_file = open( out_f, 'w', encoding="utf-8")
             sen_prpty_file = open( out_f2, 'w', encoding="utf-8")
 
             test_dataset = mydata.Dataset(te_sents,te_targets, words.token_to_ix, senses.token_to_ix, fragment.token_to_ix, integration_labels.token_to_ix, tokenizer, device, content_frg_idx, orgn_sents)
 
-            test_loader = data.DataLoader(dataset=test_dataset, batch_size=hyper_batch_size, shuffle=False, collate_fn=my_collate)
+            test_loader = data.DataLoader(dataset=test_dataset, batch_size=batch, shuffle=False, collate_fn=my_collate)
 
             for idx, (bert_input, valid_indices, target_s, target_f, target_i, og_sents) in enumerate(test_loader):
 
@@ -364,7 +332,7 @@ if __name__ == '__main__':
             #python counter.py -f1 prediction_test.txt -f2 test.txt -prin -ms_file result_test.txt -g clf_signature.yaml
 
                 for sen, tar_s, tar_f, tar_i in zip(og_sents,sense_pred,frg_pred,inter_pred):
-                    ana_clauses = decode(sen, [tuple_to_dictlist(t_s) for t_s in tar_s], [tuple_to_list(t_f) for t_f in tar_f], [tuple_to_iterlabels(t_i) for t_i in tar_i], words.token_to_ix, count, pred_file, lang)
+                    ana_clauses = decode(sen, [tuple_to_dictlist(t_s) for t_s in tar_s], [tuple_to_list(t_f) for t_f in tar_f], [tuple_to_iterlabels(t_i) for t_i in tar_i], words.token_to_ix, count, pred_file, language)
                     #sen_prpty_file.write(str(count)+"\t"+str(len(sen))+"\n") # for relation between number of words and fscore
                     sen_prpty_file.write(ana_metrics(ana_clauses, count))  # for eval fscore of drs contains certain clause element                 
                     count+=1
@@ -374,6 +342,44 @@ if __name__ == '__main__':
             print("Sense Accurancy: ", correct_s/n_of_t)
             print("Fragment Accurancy: ", correct_f/n_of_t)
             print("intergration label Accurancy: ", correct_i/n_of_t)
+    return language, batch, train, epoch, num_warmup_steps, learning_rate, finetuning, save_checkpoint, model_file, dev_file, test_file, train_file, train_file_op1, train_file_op2
+
+
+
+def my_collate(batch):
+
+    input_ids = [item[0] for item in batch]
+    token_type_ids = [item[1] for item in batch]
+    attention_mask = [item[2] for item in batch]
+    valid_indices = [item[3] for item in batch]
+                
+    input_ids = pad_sequence(input_ids, batch_first=True, padding_value=0.0)
+    token_type_ids = pad_sequence(token_type_ids, batch_first=True, padding_value=0.0)
+    attention_mask = pad_sequence(attention_mask, batch_first=True, padding_value=0.0)
+
+    bert_input = {'input_ids':input_ids, 'token_type_ids':token_type_ids, 'attention_mask':attention_mask}
+
+    target_s = [torch.LongTensor(item[4]).to(device) for item in batch]
+    target_f = [torch.LongTensor(item[5]).to(device) for item in batch]
+    target_i = [torch.LongTensor(item[6]).to(device) for item in batch]
+    orgn_sent = [item[7] for item in batch]
+
+    return bert_input, valid_indices, target_s, target_f, target_i, orgn_sent
+
+
+
+
+def average_word_emb(emb, valid):
+    embs = []
+    for i in range(len(valid)-1):
+        embs.append(emb[torch.LongTensor([idx for idx in range(valid[i], valid[i+1])]).to(device)].mean(0).unsqueeze(0))
+    return torch.cat(embs, 0)
+
+if __name__ == '__main__':
+
+    #train
+    config()
+
 
 
 
